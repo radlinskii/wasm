1d1c2094c63cb8c4f95f6d0fcb7f0b823f617e63
radlinskii <radlinskiignacy@gmail.com>
2019-06-03 08:11:29 +0200

    passing values between JS and C

--- lib/demo.c
+++ lib/demo.c
@@ -1,7 +1,33 @@
+#include <emscripten.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Hello WASM World!\n");
+    printf("Hello from WebAssembly!\n");
 
-    return 1;
+    char * name = "'ignacy'";
+    char script[50];
+
+    sprintf(script, "greetFromJS(%s", name);
+
+    char * string = emscripten_run_script_string("greetFromJS('ignacy')");
+
+    printf("%s\n", string);
+
+    return 0;
+}
+
+int getNum() {
+    return 42;
+}
+
+int getDoubleNum(int n) {
+    return n*2;
+}
+
+char * greet(char * name) {
+    char greeting[50];
+    sprintf(greeting, "Hello %s, from C!\n", name);
+
+    return greeting;
 }
