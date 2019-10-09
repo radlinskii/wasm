fe3c6803cf95f1c938baa4f0acce92bc06ecf48f
radlinskii <radlinskiignacy@gmail.com>
2019-10-09 18:18:50 +0200

    rename demo.cpp to main.cpp

--- buildwasm.sh
+++ buildwasm.sh
@@ -1,1 +1,1 @@
-emcc ./src/lib/demo.cpp -s FORCE_FILESYSTEM=1 -s WASM=1 -O2 -s EXPORTED_FUNCTIONS="['_getNum', '_main', '_getDoubleNum', '_greet']" -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -o ./public/wasm/demo.js
+emcc ./src/lib/main.cpp -s FORCE_FILESYSTEM=1 -s WASM=1 -O2 -s EXPORTED_FUNCTIONS="['_getNum', '_main', '_getDoubleNum', '_greet']" -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -o ./public/wasm/main.js
